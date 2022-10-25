08889e86f9bcf8679848672b15901625352822a6
allancodenet <allancodenet@gmail.com>
2022-10-25 23:33:47 +0300

    added active storage support and changed naming of streams

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :substreams
+      resources :students
+      resources :streams
+
+      root to: "substreams#index"
+    end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
